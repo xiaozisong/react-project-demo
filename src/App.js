39e1b1160d8c91f5cbf8e410ee19290f913c100d
xiaozisong <1170957523@qq.com>
2022-05-29 22:11:49 +0800

    路由懒加载

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import { Router, Route, Switch, Redirect } from 'react-router-dom'
-import Login from '@/pages/Login';
-import Layout from '@/pages/Layout';
-import NotFound from '@/pages/NotFound';
+import { lazy, Suspense } from 'react'
+
 import { createBrowserHistory } from 'history'
 import AuthRoute from './components/AuthRoute';
 export const history = createBrowserHistory()
+
+const Layout = lazy(() => import('@/pages/Layout'))
+const Login = lazy(() => import('@/pages/Login'))
+const NotFound = lazy(() => import('@/pages/NotFound'))
 function App() {
   console.log(history, 'APP')
   return (
     <Router history={history}>
-      <Switch>
-        <Redirect exact from='/' to='/home'></Redirect>
-        {/* 实现路由前置守卫 */}
-        {/* <Route path='/home'  render={() => {
-          if(getToken()) {
-            return <Layout></Layout>
-          } else {
-            history.replace('/login')
-          }
-        }}></Route> */}
-        <Route path="/login" component={Login}></Route>
-        {/* 将路由组件封装到组件中 */}
-        <AuthRoute path='/home' component={Layout}></AuthRoute>
-        <Route component={NotFound}></Route>
-      </Switch>
+      <Suspense fallback={<div>loading</div>}>
+        <Switch>
+          <Redirect exact from='/' to='/home'></Redirect>
+          {/* 实现路由前置守卫 */}
+          {/* <Route path='/home'  render={() => {
+            if(getToken()) {
+              return <Layout></Layout>
+            } else {
+              history.replace('/login')
+            }
+          }}></Route> */}
+          <Route path="/login" component={Login}></Route>
+          {/* 将路由组件封装到组件中 */}
+          <AuthRoute path='/home' component={Layout}></AuthRoute>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Suspense>
+      
     </Router>
   );
 }
